a9cae2b1581335323fcf0cbb56f3785a5f1eed40
= <wjdrlans4000@naver.com>
2021-03-28 00:07:05 +0900

    remove springinit folder

--- springinit/src/main/java/com/example/springinit/a/AccountService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springinit.a;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AccountService {
-}
--- springinit/src/main/java/com/example/springinit/b/BookConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.example.springinit.b;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BookConfiguration {
-}
