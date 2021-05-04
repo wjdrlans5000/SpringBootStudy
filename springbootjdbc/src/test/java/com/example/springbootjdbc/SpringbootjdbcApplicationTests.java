52c46b3989333400b652e196f9d0d83830200f87
= <wjdrlans4000@naver.com>
2021-05-04 23:15:42 +0900

    SPRING 인메모리 데이터베이스 H2사용

---
+++ springbootjdbc/src/test/java/com/example/springbootjdbc/SpringbootjdbcApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.springbootjdbc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjdbcApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
